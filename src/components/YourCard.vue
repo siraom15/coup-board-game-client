c7488aec02d3c2f535f05101ee008368263aa83f
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-03-23 17:41:55 +0700

    Update  Responsive🍻

--- src/components/YourCard.vue
+++ src/components/YourCard.vue
@@ -6,13 +6,13 @@
       src="/cards/ass.png"
       alt=""
       srcset=""
-      class="w-28 sm:w-32 md:w-36 lg:w-40 xl:w-56"
+      class="w-28 sm:w-32 md:w-36 lg:w-40 xl:w-56 2xl:w-64"
     />
     <img
       src="/cards/cont.png"
       alt=""
       srcset=""
-      class="w-28 sm:w-32 md:w-36 lg:w-40 xl:w-56"
+      class="w-28 sm:w-32 md:w-36 lg:w-40 xl:w-56 2xl:w-64"
     />
   </div>
 </template>
